a231138fd5f4c821c7ae5221d3058f6dd15a1854
Fedenoodt <fedenoodtmolins@gmail.com>
2021-07-22 19:26:14 -0300

    Practicando ejercicios simples

--- Proyecto Zero/Programación/Python/pythondiario/5.py
+++ Proyecto Zero/Programación/Python/pythondiario/5.py
@@ -1,18 +1,24 @@
 numeros = []
 numero = ''
 
-def lectura(numeros):
-    for i in range (0, len(numeros) - 1):
-        suma = numeros[i] + numeros[i + 1]
+def sumar(numeros):
+    suma = 0
+    for i in numeros:
+        suma += i
     return suma
 
+def multiplicar(numeros):
+    multiplicacion = 1
+    for i in numeros:
+        multiplicacion *= i
+    return multiplicacion
 
 while numero != '*':
     numero = input('Ingrese un número ("*" para finalizar.) :')
     if numero != '*':
         numeros.append(int(numero))
 
-suma = lectura(numeros)
-
-print(len(numeros))
-#print(numeros, '\n' + str(suma))
+suma = sumar(numeros)
+multiplicacion = multiplicar(numeros)
+print(suma)
+print(multiplicacion)
---
+++ Proyecto Zero/Programación/Python/pythondiario/6.py
@@ -0,0 +1,2 @@
+cadena = input('Ingrese cualquier frase.\n\n\n')
+
